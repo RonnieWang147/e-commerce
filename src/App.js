03030714c155fe27aef82bc1e5e84a4c4d442332
Yue Wang <yuewang@Lulus-MBP.home>
2019-07-06 15:01:07 -0400

    Add react-router to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
+const Hats = () => <h1>HATS PAGE</h1>;
+
 function App() {
-  return <HomePage />;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={Hats} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Directory from '../../components/directory/directory.component';
-import MenuItem from '../../components/menu-item/menu-item.component';
 import './homepage.styles.scss';
 
 const HomePage = props => (
   <div className="homepage">
+    <Link to="/hats">to hats</Link>
     <Directory />
   </div>
 );
